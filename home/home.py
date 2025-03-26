ed9d385b1519bceaac9e5139d5f3a2b859ec1ff0
Isak-Landin <utbisalan@gmail.com>
2025-03-26 19:28:49 +0100

    Auto commit

--- home/home.py
+++ home/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-home_bp = Blueprint('home', __name__, template_folder='templates')
+home_bp = Blueprint('home', __name__, template_folder='static/templates')
 
 
 @home_bp.route('/home')
